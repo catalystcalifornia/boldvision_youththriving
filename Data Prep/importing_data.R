ec026d26b11b56c5ffae0d9fea78b96a62bf17ff
Elycia Mulholland Graves <egraves@advanceproj.org>
2024-10-02 14:44:27 -0700

    Creating a subfolder and moving script for data importing. Added additional code for cleaning up column content and added primary key for postgres

--- Data Prep/importing_data.R
+++ Data Prep/importing_data.R
@@ -19,7 +19,7 @@ source("W:\\RDA Team\\R\\credentials_source.R")
 con_bv <- connect_to_db("bold_vision") 
 
 
-####Step 2: Read in Dataset and sent to Database with comments ####
+####Step 2: Read in dataset and sent to database with comments ####
 ys_data <- read_excel("W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Data\\Survey responses\\Final Data\\BVYTSWeightSummary_Database.xlsx",sheet="Database")
 
 ys_data<-clean_names(ys_data)
@@ -36,13 +36,14 @@ colnames(ys_data)[grepl('q10b_which_of_the_following_',colnames(ys_data))] <- 'q
 colnames(ys_data)[grepl('q10a_how_many_adults_really_',colnames(ys_data))] <- 'q10a'
 
 
-dbWriteTable(con_bv, c('youth_thriving', 'raw_survey_data'), ys_data,
-                          overwrite = FALSE, row.names = FALSE)
+# dbWriteTable(con_bv, c('youth_thriving', 'raw_survey_data'), ys_data,
+#                           overwrite = FALSE, row.names = FALSE)
 
-dbSendQuery(con_bv, "COMMENT ON TABLE youth_thriving.raw_survey_data IS 'The following dataset are responses from the Youth Thriving Survey conducted by Bold Vision in 2024. The data dictionary explaining each variable is here: youth_thriving.bvys_datadictionary_2024 . Steps explaining data cleaning can be found here: W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Data\\Survey responses\\Final Data\\BVYTSPopulationWeighting_DataCleaning.pdf Original Dataset is here: a)	W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Data\\Survey responses\\Final Data\\BVYTSWeightSummary_Database.xlsx The process for cleaning and uploading the data is explained in the QA Documentation here: W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Documentation\\QA_dataimport_datadictionary.docx'")
+# dbSendQuery(con_bv, "COMMENT ON TABLE youth_thriving.raw_survey_data IS 'The following dataset are responses from the Youth Thriving Survey conducted by Bold Vision in 2024. The data dictionary explaining each variable is here: youth_thriving.bvys_datadictionary_2024 . Steps explaining data cleaning can be found here: W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Data\\Survey responses\\Final Data\\BVYTSPopulationWeighting_DataCleaning.pdf Original Dataset is here: a)	W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Data\\Survey responses\\Final Data\\BVYTSWeightSummary_Database.xlsx The process for cleaning and uploading the data is explained in the QA Documentation here: W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Documentation\\QA_dataimport_datadictionary.docx'")
 
 
 
+####Step 3: Read in data dictionary and send to database comments ####
 
 data_dictionary <- read_excel("W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Data\\Survey responses\\bvys_datadictionary_2024.xlsx")
 
@@ -53,8 +54,40 @@ data_dictionary <- data_dictionary %>% mutate(
   mutate(primary_key = row_number()) %>%
   select(primary_key, everything())
 
-dbWriteTable(con_bv, c('youth_thriving', 'bvys_datadictionary_2024'), data_dictionary,
-             overwrite = TRUE, row.names = FALSE)
+# generalize variables in Caring Families and Relationships to align with conceptboard
+data_dictionary$variable_name[data_dictionary$variable_name %in% c("Support: Adults, Family, Peers (Aligns With Youth Voice)","School Relationships")] <- "Relationships/Support"
+
+# match Demographic to Demographics in response domains
+data_dictionary$response_domain[data_dictionary$response_domain=="Demographic"] <- "Demographics"
+
+# specify Info domain for org
+data_dictionary$response_domain[data_dictionary$variable=="org"] <- "Info"
+
+# do some other clean up for consistent labels
+data_dictionary<-data_dictionary%>%mutate(variable_name=str_replace(variable_name," Nhpi "," NHPI "),
+                                          variable_name=str_replace(variable_name,"Spa ","SPA "))
+
+
+data_dictionary[data_dictionary=="Don't Wish to Answer"]<-"Don't wish to answer"
+data_dictionary[data_dictionary=="Don't Wish to Answer"]<-"Don't wish to answer"
+data_dictionary[data_dictionary=="Dont wish to answer"]<-"Don't wish to answer"
+data_dictionary[data_dictionary=="Dont Wish to Answer"]<-"Don't wish to answer"
+data_dictionary[data_dictionary=="I am not sure or Questioning"]<-"I am not sure or questioning"
+data_dictionary[data_dictionary=="Dont know"]<-"Don't know"
+data_dictionary[data_dictionary=="Don't Know"]<-"Don't know"
+data_dictionary[data_dictionary=="Dont Know"]<-"Don't know"
+data_dictionary[data_dictionary=="South West Asian and North African"]<-"Southwest Asian and North African"
+data_dictionary[data_dictionary=="some of the time"]<-"Some of the time"
+data_dictionary[data_dictionary=="most of the time"]<-"Most of the time"
+data_dictionary[data_dictionary=="all of the time"]<-"All of the time"
+data_dictionary[data_dictionary=="Never True"]<-"Never true"
+data_dictionary[data_dictionary=="Sometimes True"]<-"Sometimes true"
+data_dictionary[data_dictionary=="Often True"]<-"Often true"
+data_dictionary[data_dictionary=="Always True"]<-"Always true"
+
+
+# dbWriteTable(con_bv, c('youth_thriving', 'bvys_datadictionary_2024'), data_dictionary,
+#              overwrite = FALSE, row.names = FALSE)
 
 dbSendQuery(con_bv, "COMMENT ON TABLE youth_thriving.bvys_datadictionary_2024 IS 'The following data dictionary aims to decode the data for the Bold Visin Youth Thriving Survey Data for 2024. QA Documentation here: W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Documentation\\QA_dataimport_datadictionary.docx'")
 
@@ -78,7 +111,10 @@ dbSendQuery(con_bv, "COMMENT ON COLUMN youth_thriving.bvys_datadictionary_2024.v
                      COMMENT ON COLUMN youth_thriving.bvys_datadictionary_2024.response_10 IS 'what the response is if the variable has a 10 coded for the data.';
                      COMMENT ON COLUMN youth_thriving.bvys_datadictionary_2024.response_11 IS 'what the response is if the variable has a 11 coded for the data.';
                      COMMENT ON COLUMN youth_thriving.bvys_datadictionary_2024.response_12 IS 'what the response is if the variable has a 12 coded for the data.';
-                     COMMENT ON COLUMN youth_thriving.bvys_datadictionary_2024.notes IS 'any important notes like what variables were just for the weights';
+                     COMMENT ON COLUMN youth_thriving.bvys_datadictionary_2024.notes IS 'any important notes like what variables were just for the weights and whether skip logic was applied';
             ")
+
+# add primary key for editing in database
+dbSendQuery(con_bv, "ALTER TABLE youth_thriving.bvys_datadictionary_2024 ADD PRIMARY KEY (primary_key)");
 
 dbDisconnect(con_bv)
