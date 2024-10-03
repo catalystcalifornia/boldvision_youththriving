52129049f2fc75e6f27af3bee99037324bf20ca2
Maria Khan <MKhan@catalystcalifornia.org>
2024-10-03 15:05:39 -0700

    Analysis by race an chi_square testing

---
+++ analysis_functions.R
@@ -0,0 +1,8 @@
+#The purpose of this R script is to store analysis functions for the bold vision youth thriving survey project. 
+
+
+#### Function: Analysis by Race and Question, produces tables ####
+
+
+
+#### Function: Chi-Squared Test Analysis by Race ####
