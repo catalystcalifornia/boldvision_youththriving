9e539d18960bd5b7c7ed720e3539d3cdbe3395be
Elycia Mulholland Graves <egraves@advanceproj.org>
2024-10-04 12:30:30 -0700

    finalizing script and pushing to postgres

--- Survey Weighting/population_counts.R
+++ Survey Weighting/population_counts.R
@@ -75,8 +75,10 @@ sex_counts<-subset(age_sex, select=names(age_sex) %in% vars_age_sex$name)%>% # s
   mutate(percent=count/sum(count))
 
 # SPA counts ----
-#get spa shapefile and reduce fields
+## Get and prep shapes -----
+# get spa shapefile and reduce fields
 spas <- st_read(rda_con, query = "select * from geographies_la.la_county_service_planning_areas_2022")
+
 # check projection and transform
 st_crs(spas)
 spas<-st_transform(spas,3310)
@@ -106,34 +108,109 @@ tract_spa_xwalk <- st_join(spas, st_centroid(lac_tracts), join = st_contains)
 # check result
 nrow(tract_spa_xwalk) # 2494, 1 missing
 missing<-lac_tracts%>%filter(!GEOID %in% tract_spa_xwalk$GEOID)
+# mapview(missing) Just the islands didn't merge to SPA 8, merge back later
 
-tract_spa_xwalk_check<-st_transform(tract_spa_xwalk,4326)
+# convert to df and reduce columns
+tract_spa_xwalk <- as.data.frame(tract_spa_xwalk%>%st_drop_geometry()) %>% select(GEOID, SPA, SPA_NAME,LABEL) %>% 
+  rename(geoid = GEOID, spa = SPA, spa_name = SPA_NAME,label=LABEL)
+
+# check results through a map
+xwalk_check<-lac_tracts%>%left_join(tract_spa_xwalk,by=c("GEOID"='geoid'))
+xwalk_check<-st_transform(xwalk_check,4326)
+spa_check<-st_transform(spas,4326)
+
 # create a color palette for the map
 pal <- colorFactor(
   palette = 'Dark2',
-  domain = tract_spa_xwalk$SPA
+  domain = xwalk_check$spa_name
 )
 
 # map the data
-leaflet(bg_access) %>%
+leaflet(xwalk_check) %>%
   addTiles() %>%
   addPolygons(
     stroke = FALSE, fillOpacity = 0.5,
-    smoothFactor = 0.5,  fillColor = ~ mypalettez(indicator_final),
+    smoothFactor = 0.5,  fillColor = ~ pal(spa_name),
   )%>%
+  addPolygons(data = spa_check,
+              weight = 1)%>%
   addLegend(
-    pal = mypalettez, values = ~indicator_final, opacity = 0.9,
+    pal = pal, values = ~spa_name, opacity = 0.9,
     position = "bottomleft"
   )
+  
+# add back one tract that fails in spatial join
+tract_spa_xwalk[nrow(tract_spa_xwalk) + 1, ] = c("06037599100", "8", "South Bay","SPA 8")
 
-#convert to df and reduce columns
-tract_spa_xwalk <- as.data.frame(tract_spa_xwalk) %>% select(GEOID, SPA, SPA_NAME) %>% 
-  rename(geoid = GEOID, spa = SPA, spa_name = SPA_NAME)
+## Calculate 15-24 population by SPA ----
+# pull data from dataframe
+spa_counts_step1<-age_sex_tract%>%select(geoid, all_of(vars_age$name)) %>% # subset variables to those we need matching df above
+  left_join(tract_spa_xwalk) # join SPA matches
 
-#add back one tract that fails in spatial join
-tract_spa_xwalk[nrow(tract_spa_xwalk) + 1, ] = c("06037599100", "8", "South Bay")
+spa_counts<-spa_counts_step1%>% 
+  group_by(spa,spa_name,label)%>% # summarize 15-24 population by SPA
+  summarise(count=sum(s0101_c01_021e+s0101_c01_023e),.groups='drop')%>%
+  rename(geoid=spa, name=spa_name, variable=label)%>%
+  mutate(weighting_group='SPA')%>%
+  mutate(percent=count/sum(count))%>%
+  filter(!is.na(variable))%>%
+  select(geoid,name,weighting_group,everything())
 
-# add missing tract
+# Bind together population counts ----
+df<-rbind(age_counts,sex_counts,spa_counts)
+
+# check result
+df%>%group_by(weighting_group)%>%summarise(pop=sum(count),total=sum(percent))
+# looks good
+
+# Push to postgres ----
+#### Relational searches stops ----
+table_name <- "acs_weighting_population_counts"
+schema <- 'youth_thriving'
+
+indicator <- "Age, sex, and SPA counts for use in survey weighting based on 5-year ACS data for LA County 2018-2022 subject table S0101"
+source <- "See QA doc for details: W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Documentation\\QA_acs_weights_population_counts.docx
+Script: W:/Project/OSI/Bold Vision/Youth Thriving Survey/GitHub/EMG/boldvision_youththriving/Survey Weighting/population_counts.R"
+table_comment <- paste0(indicator, source)
+
+# write table
+# dbWriteTable(con, c(schema, table_name),df,
+#              overwrite = FALSE, row.names = FALSE)
+
+# comment on table and columns
+column_names <- colnames(df) # get column names
+
+column_comments <- c(
+  "GEOID or object ID--mostly LA County with IDs for SPAs for SPA level weighting",
+  "name of county or SPA",
+  "Weighting level-age, sex, or SPA",
+  "Category or variable for that weighting level",
+  "Total estimated count of target youth population in category or variable",
+  "Percent of target youth population in category or variable out of youth population for that weighting group"
+)
+
+add_table_comments(con, schema, table_name, indicator, source, column_names, column_comments)
 
 #disconnect
 dbDisconnect(con)
+
+# Push 2022 tracts to postgres because they aren't there
+cb_2022_06_tract_500k<-ca_tracts
+
+# # not working
+# st_write(cb_2022_06_tract_500k, dsn = rda_con, schema="geographies_ca",layer = "cb_2022_06_tract_500k",
+#          delete_layer = FALSE, append = FALSE)
+
+indicator <- "2022 Tract Boundaries for California from TIGRIS package"
+source <- "See QA doc for details: W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Documentation\\QA_acs_weights_population_counts.docx
+Script: W:/Project/OSI/Bold Vision/Youth Thriving Survey/GitHub/EMG/boldvision_youththriving/Survey Weighting/population_counts.R"
+table_comment <- paste0(indicator, source)
+
+
+# add table comment
+# dbSendQuery(rda_con, paste0("comment on table geographies_ca.cb_2022_06_tract_500k is
+#                         '2022 Tract Boundaries for California from TIGRIS package
+#                           See QA doc for details: W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\Documentation\\QA_acs_weights_population_counts.docx
+#                           Script: W:/Project/OSI/Bold Vision/Youth Thriving Survey/GitHub/EMG/boldvision_youththriving/Survey Weighting/population_counts.R';"))
+
+dbDisconnect(rda_con)
