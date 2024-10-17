e5317c336b6ce372e0b1a280eeec28cb7ce9e5db
Elycia Mulholland Graves <egraves@advanceproj.org>
2024-10-17 14:51:32 -0700

    removing the cv filter from visual functions for now because it was throwing off the group by statements. Also editing titles and categories a bit. some likert scales had slightly different wording than the codebook

--- visual_outputs.R
+++ visual_outputs.R
@@ -1,17 +1,17 @@
 #source script
 
-source('W:\\Project\\OSI\\Bold Vision\\Youth Thriving Survey\\GitHub\\MK\\boldvision_youththriving\\visual_functions.R')
+source('visual_functions.R')
 
 #visualizing key findings for Youth Council presentation 
 
 fx_single_barchart(question_number_i = '18', sub_question_i = 'Contact with the police', #these are the inputs, i stands for insert/input of the variable of interest we want to look at
                    response_label_1 = 'Sometimes', response_label_2 = 'Most of the time', response_label_3 = 'All of the time', #insert the responses we want to capture data for, up to 3
-                   title_text = 'Contact with police is interfering with Black youth at a rate higher than any other group', x_axis_text = 'Rate of youth reporting contact in police getting in the way of living their best life' #insert text on graph that is customized
+                   title_text = 'Contact with police is interfering with Black youth at a rate higher than most groups', x_axis_text = 'Rate of youth reporting contact in police getting in the way of living their best life' #insert text on graph that is customized
 )  
 
 fx_single_barchart(question_number_i = '16', sub_question_i = 'At school', #these are the inputs, i stands for insert/input of the variable of interest we want to look at
                    response_label_1 = 'Sometimes', response_label_2 = 'Most of the time', response_label_3 = 'All of the time', #insert the responses we want to capture data for, up to 3
-                   title_text = 'Black youth experience racial discrimintation at schools at a rate higher than any other group', x_axis_text = 'Rate of youth reported being treated unfairly due to their race at school' #insert text on graph that is customized
+                   title_text = 'Black youth experience racial discrimination at schools at a rate higher than any other group', x_axis_text = 'Rate of youth reported being treated unfairly due to their race at school' #insert text on graph that is customized
 )  
 
 fx_single_barchart_freq(domain_pgname = 'safety', #name of domain as it is in the pgadmine table// for example, "strong_minds"
@@ -30,8 +30,8 @@ fx_single_barchart_freq(domain_pgname = 'caring_families', #name of domain as it
 )
 
 fx_single_barchart(question_number_i = '9', sub_question_i = 'Nervous', #these are the inputs, i stands for insert/input of the variable of interest we want to look at
-                   response_label_1 = 'Sometimes', response_label_2 = 'Most of the time', response_label_3 = 'All of the time', #insert the responses we want to capture data for, up to 3
-                   title_text = 'Over 40% of NHPI, Multiracial, and Asian youth feel nervous some of the time', x_axis_text = 'Rate of youth reporting feelings of nervousness in the past year' #insert text on graph that is customized
+                   response_label_1 = 'Some of the time', response_label_2 = 'Most of the time', response_label_3 = 'All of the time', #insert the responses we want to capture data for, up to 3
+                   title_text = 'Over 60% of Multiracial, Asian, and Latine youth feel nervous some of the time', x_axis_text = 'Rate of youth reporting feelings of nervousness in the past year' #insert text on graph that is customized
 )  
 
 fx_single_barchart(question_number_i = '11', sub_question_i = 'I feel I have time to figure out what I like', #these are the inputs, i stands for insert/input of the variable of interest we want to look at
@@ -47,5 +47,5 @@ fx_single_barchart(question_number_i = '8', sub_question_i = 'I believe that I a
 
 fx_single_barchart_freq(domain_pgname = 'cultural_identity', #name of domain as it is in the pgadmine table// for example, "strong_minds"
                         variable_i = 'dz', #these are the inputs, i stands for insert/input of the variable of interest we want to look at
-                        title_text = 'Over half report the importance of their cultural identity to feeling good about who they are', x_axis_text = '' #insert text on graph that is customized
+                        title_text = 'Over half of youth report the importance of their cultural identity to feeling good about who they are', x_axis_text = '' #insert text on graph that is customized
 )
