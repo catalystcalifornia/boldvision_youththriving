40a95b0d4a764064cea6727f53e398c4f663bb5f
Maria Khan <MKhan@catalystcalifornia.org>
2024-10-29 17:05:16 -0700

    prepping analysis correlation, still work in progress to get average scores across subcomponent and component

---
+++ analysis_correlation.R
@@ -0,0 +1,90 @@
+## The following R script is created to run a correlation analysis to uderstand the relationship between the different variables measured in the Bold Vision Youth Thriving Survey.
+
+## Author: Maria Kha 
+
+#### Step 0: Setting Up Workspace ####
+library(data.table)
+library(dplyr)
+library(RPostgreSQL)
+library(tidyr)
+library(readxl)
+library(tidyverse)
+library(srvyr)
+library(survey)
+library(devtools) 
+options(scipen=999)
+
+# connect to postgres and source functions
+source("W:\\RDA Team\\R\\credentials_source.R")
+
+con <- connect_to_db("bold_vision")
+
+svy_data <- dbGetQuery(con, "SELECT * FROM youth_thriving.raw_survey_data")
+svy_dd <- dbGetQuery(con, "SELECT * FROM youth_thriving.bvys_datadictionary_2024 where response_domain !='Demographics' AND response_domain != 'Info'")
+
+
+#### Step 1: Create a datatable for postgres averaging scores per component and subcomponent for each survey respondent ####
+
+## Step 1A: Create a reference table for how scoring will be done ##
+
+true_factors<-c("Never true","Sometimes true","Often true","Always true","Don't wish to answer") # most commonly used factor here
+time_factors<-c("None of the time","A little of the time","Some of the time","Most of the time","All of the time","Don't wish to answer")
+yes_factors<-c("Yes","No","Not sure","Don't wish to answer")
+yes_factors_2<-c("No","Yes","Dont Know","Don't wish to answer")
+count_factors<-c("None","One","Two","Three or more","Not Sure","Don't wish to answer")
+frequency_factors<-c("Never","Rarely","Sometimes","Most of the time","All of the time","Does not apply to me")
+frequency_factors_2<-c("Never","Rarely","Sometimes","Most of the time","All of the time","Don't wish to answer","Does not apply to me")
+
+
+# Filter out unwanted levels and create continuous scales
+true_factors_cont <- factor(true_factors, levels = c("Never true", "Sometimes true", "Often true", "Always true"))
+true_scale <- as.numeric(true_factors_cont)  # 1 to 4 scale
+
+time_factors_cont <- factor(time_factors, levels = c("None of the time", "A little of the time", "Some of the time", "Most of the time", "All of the time"))
+time_scale <- as.numeric(time_factors_cont)  # 1 to 5 scale
+
+yes_factors_cont <- factor(yes_factors, levels = c("Yes", "No"))
+yes_scale <- ifelse(yes_factors_cont == "Yes", 1, 0)  # Binary scale
+
+count_factors_cont <- factor(count_factors, levels = c("None", "One", "Two", "Three or more"))
+count_scale <- as.numeric(count_factors_cont)  # 1 to 4 scale
+
+frequency_factors_cont <- factor(frequency_factors, levels = c("Never","Rarely","Sometimes","Most of the time","All of the time"))
+frequency_scale <- as.numeric(frequency_factors_cont) # 1 to 4 scale
+
+# View the resulting scales
+print(true_scale)      # Outputs 1 to 4 scale for "true_factors" levels
+print(time_scale)      # Outputs 1 to 5 scale for "time_factors" levels
+print(yes_scale)       # Outputs binary scale for "yes_factors" levels
+print(count_scale)     # Outputs 1 to 4 scale for "count_factors" levels
+print(frequency_scale) # Outputs 1 to 4 scale for "frequency_factors" levels
+
+#### Step 1B: Run scoring for one subcomponent #### 
+
+dict <- svy_dd %>% filter(variable == 'cn') %>%
+  pivot_longer(cols = response_1: response_12,
+               names_to = "response_numbers",
+               values_to = paste0("response"),
+               values_drop_na = TRUE)
+dict_var <- dict %>%
+  mutate(variable_factor_level = 1:nrow(dict)) 
+
+svy_data_var <- svy_data %>% 
+  select(c('response_id', 'cn'))
+
+df_almost_final <-  merge(x = svy_data_var, y = dict_var, by.x = c("cn"), by.y = c("variable_factor_level"))
+
+true_factors_test <-c("Never true","Sometimes true","Often true","Always true") 
+
+df_final <- df_almost_final %>% 
+  mutate(response = factor(response, levels = true_factors_test)) 
+
+##next step is updating bv data dictionary and then selecting only columns needed and averaging by subcomponent and then component
+
+
+
+
+
+
+
+
