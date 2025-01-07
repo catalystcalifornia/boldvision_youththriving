92c12df3f857d599d2b2d52d3b37bb0ddba1b093
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-01-06 16:21:48 -0800

    small parsing and punctuation to caption

--- Visuals/visual_functions.R
+++ Visuals/visual_functions.R
@@ -172,8 +172,8 @@ fx_single_barchart <- function(question_number_i, sub_question_i, #these are the
          y = "",
          caption= paste(str_wrap(paste0("Question: ", unique(df_filter$question), " ",
                                         unique(df_filter$sub_question), ".\n",
-                                        " Component: ", unique(df_filter$response_domain), "\n",
-                                        " Subcomponent: ", unique(df_filter$variable_name), "\n",
+                                        " Component: ", unique(df_filter$response_domain), ",\n",
+                                        " Subcomponent: ", unique(df_filter$variable_name), ".\n",
                                         " Data Source: Bold Vision Youth Thriving Survey, 2024."),
                                  whitespace_only = TRUE, width = 120), collapse = "\n")) +
     #theme/aesthetics
