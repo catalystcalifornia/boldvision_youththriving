5cb3ca81790e06702b8ffe817f8fca75f6caa9c4
Maria Khan <MKhan@catalystcalifornia.org>
2025-02-14 15:22:21 -0800

    more visual template updates for small multiples

--- Visuals/visual_smallmultiples.R
+++ Visuals/visual_smallmultiples.R
@@ -20,6 +20,7 @@ library(scales)
 library(kableExtra)
 library(flextable)
 library(ggchicklet)
+library(GGridge)
 
 ##Colors
 gray <- "#D6D7D6"
@@ -52,12 +53,12 @@ font_axis_label <- "HelveticaNeueLTStdMdCn"
 source("W:\\RDA Team\\R\\credentials_source.R")
 con <- connect_to_db("bold_vision")
 
-fx_create_df <- function(con, tables, response_domain, variable, response) {
+fx_create_df <- function(con, tables, response_domain, variable) {
   
   # Function to fetch data from a single table
   fetch_data <- function(table) {
-    query <- sprintf("SELECT * FROM youth_thriving.%s WHERE response_domain = '%s' AND variable = '%s' AND response = '%s'", 
-                     table, response_domain, variable, response)
+    query <- sprintf("SELECT * FROM youth_thriving.%s WHERE response_domain = '%s' AND variable = '%s'",
+                     table, response_domain, variable)
     
     # Use tryCatch to handle potential query errors
     df <- tryCatch(
@@ -76,7 +77,12 @@ fx_create_df <- function(con, tables, response_domain, variable, response) {
     
     # Add 'source_table' and 'youth' columns
     df <- df %>%
-      mutate(source_table = table)
+      mutate(source_table = table,
+             youth_label = str_replace_all(youth, "_", " ") %>%  # Replace underscores with spaces
+               str_to_title(),  # Capitalize first letter of each word
+             youth_label = if_else(str_detect(youth_label, "Youth$"), youth_label, paste(youth_label, "Youth"))
+      )
+    
     
     return(df)
   }
@@ -94,24 +100,72 @@ tables <- c("tot_freq_bipoc", "tot_freq_disconnected", "tot_freq_lgbtqia",
             "tot_freq_suspended", "tot_freq_systems_impacted", 
             "tot_freq_undocumented", "tot_freq_unhoused")
 # Running function
-df_combined <- fx_create_df(con, tables, "Vibrant Communities", "ds", "Yes") 
+df_combined <- fx_create_df(con, tables, "Vibrant Communities", "ds") 
 
 #Adding all youth data 
 tot_freq_all_youth <- dbGetQuery(con, "SELECT response, frequency AS count, weighted_percent AS rate, percent_cv AS rate_cv, variable, question, sub_question, variable_name, domain AS response_domain
-                                 FROM youth_thriving.tot_freq_vibrant_community WHERE variable = 'ds' AND response = 'Yes'") %>%
+                                 FROM youth_thriving.tot_freq_vibrant_community WHERE variable = 'ds'") %>%
   mutate(youth = "all youth",
+         youth_label = "All Youth",
          source_table = "tot_freq_vibrant_community")
 
 #combine with other data 
 df_combined <- bind_rows(df_combined, tot_freq_all_youth)
 
-ggplot(df_combined, aes(x = youth, y = count, fill = source_table)) +
+df_filtered <- df_combined %>%
+  filter(!grepl("^not ", .[[1]], ignore.case = TRUE)) %>%  # Filters out rows where first column starts with "not "
+mutate(response = str_wrap(response, width = 8))
+
+#visual
+df_visual <- ggplot(df_filtered, aes(x = response, y = rate, fill = youth)) +
   geom_bar(stat = "identity") +  # Use identity to plot actual counts
-  facet_wrap(~ source_table, scales = "free_y") +  # Create small multiples
-  labs(
-    title = "Small Multiple Bar Graphs by Table Name",
-    x = "Youth",
-    y = "Count"
-  ) +
+  # scale_fill_manual(values = c(
+  #   "All Youth" = gray,
+  #   "Bipoc Youth" = pink,
+  #   "Disconnected Youth" = dark_pink,
+  #   "Lgbtqia Youth" = orange,
+  #   "Suspended Youth" = yellow,
+  #   "Systems Impacted Youth" = light_green,
+  #   "Undocumented Youth" = blue,
+  #   "Unhoused Youth" = light_blue
+  # )) +
+  facet_wrap(~ youth_label, scales = "free_y", nrow = 2) +  # Create small multiples
+  #bar labels
+  geom_text(data = df_filtered,
+            aes(label = paste0(round(rate, digits = 1), "%")),
+            size = 4,
+            stat="identity", colour = "black",
+            # position = position_dodge(width = 1), 
+            # vjust = -.20 , 
+            # hjust= 1.15,
+            fontface = "bold", family=font_bar_label) +  
   theme_minimal() +
-  theme(legend.position = "none")  # Remove legend since bars are labeled
+  labs(title = paste(str_wrap("Unhoused youth are least likely to report access to Libraries", whitespace_only = TRUE, width = 75), collapse = "\n"),
+       x = paste(str_wrap("Youth Thriving Survey Responses", whitespace_only = TRUE, width = 95), collapse = "\n"),
+       y = "",
+       caption= paste(str_wrap(paste0("Question: ", unique(df_filtered$question), " ",
+                                      unique(df_filtered$sub_question), ".\n",
+                                      " Component: ", unique(df_filtered$response_domain), ",\n",
+                                      " Subcomponent: ", unique(df_filtered$variable_name), ".\n",
+                                      " Data Source: Bold Vision Youth Thriving Survey, 2024."),
+                               whitespace_only = TRUE, width = 165), collapse = "\n")) +
+  theme(legend.position = "none",
+     # define style for axis text
+     axis.text.x = element_text(hjust = 0.5, vjust = 1, lineheight = .75),  # x-axis labels
+     axis.text.y = element_text(size = 9, colour = "black", family= font_axis_label, face = "bold"),
+     axis.title.x = element_text(size = 12, colour = "black", family = font_axis_label, face = "bold", margin = margin(t = 5)),
+     # define style for title and caption
+     plot.caption = element_text(hjust = 0.0, size = 8, colour = "black", family = font_caption, face = "plain"),
+     plot.title = element_text(hjust = 0.0, size = 18, colour = "black", family = font_title, face = "bold"),
+     # grid line style
+     panel.grid.minor = element_blank(),
+     panel.grid.major = element_blank()) 
+
+df_visual
+
+ggsave(plot=df_visual, 
+       file=paste0("W:/Project/OSI/Bold Vision/Youth Thriving Survey/Deliverables/testing_small_multiples", ".svg"),
+       units = c("in"),  width = 8, height = 5.5)
+
+
+
