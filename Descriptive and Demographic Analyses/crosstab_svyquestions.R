7df5dc9c79fd78079aca797bfba73dd3cf3c7e57
Maria Khan <MKhan@catalystcalifornia.org>
2025-02-21 14:44:43 -0800

    added some sophistication to my function by including data dictionary to explain what the variables actually are, hope that is helpful!

--- Descriptive and Demographic Analyses/crosstab_svyquestions.R
+++ Descriptive and Demographic Analyses/crosstab_svyquestions.R
@@ -6,6 +6,7 @@
 library(dplyr)
 library(RPostgreSQL)
 library(srvyr)
+library(stringr)
 options(scipen=999)
 
 # connect to postgres and pull credentials
@@ -19,7 +20,7 @@ svy_dd <- dbGetQuery(con, "SELECT * FROM youth_thriving.bvys_datadictionary_2024
 
 #### STEP 2: Create a function to cross tab two different variables ####
 
-cross_tab_df <- function(data, var1, var2) {
+cross_tab_df <- function(data, var1, var2, data_dict) {
   # Convert to survey design using weights
   df_svy <- as_survey(data, weights = weights_final) %>%
     filter(!is.na(!!sym(var1)), !is.na(!!sym(var2)))  # Remove NAs
@@ -34,11 +35,48 @@ cross_tab_df <- function(data, var1, var2) {
     mutate(rate = rate * 100) %>%  # Convert to percentages
     ungroup()
   
-  return(df_result)
+  #prep work for adding meaning to data
+  dict <- data_dict %>%
+    filter(variable == var1 | variable == var2) %>%
+    pivot_longer(cols = response_1:response_12,
+                 names_to = "response_numbers",
+                 values_to = "response",
+                 values_drop_na = TRUE)
+  dict_var <- dict %>%
+    mutate(
+      response_number = as.numeric(str_extract(response_numbers, "\\d+")))  # Extract the number and convert to numeric
+  dict_var1 <- dict_var %>%
+    filter(variable == var1) %>%
+    select(response_number, response, question, sub_question, response_domain, variable_name) %>%
+    rename(response_var1 = response,
+           component_var1 = response_domain,
+           subcomponent_var1 = variable_name,
+           question_var1 = question,
+           sub_question_var1 = sub_question)
+
+  df_var1_result <- df_result %>% 
+    mutate(var1_number = .[[1]]) %>%
+    left_join(dict_var1, by = c("var1_number" = "response_number"))
+  
+  dict_var2 <- dict_var %>%
+    filter(variable == var2) %>%
+    select(response_number, response, question, sub_question, response_domain, variable_name) %>%
+    rename(response_var2 = response,
+           component_var2 = response_domain,
+           subcomponent_var2 = variable_name,
+           question_var2 = question,
+           sub_question_var2 = sub_question)
+  
+  df_var1_result_final <- df_var1_result %>%
+    mutate(var2_number = .[[2]]) %>%
+    left_join(dict_var2, by = c("var2_number" = "response_number"))
+  
+  return(df_var1_result_final)
+  
 }
 
+cross_dd_de <- cross_tab_df(svy_data, "dd", "de", svy_dd)
 
-cross_dd_de <- cross_tab_df(svy_data, "dd", "de")
 View(cross_dd_de)
 
 #### STEP 3: Close db connection ####
