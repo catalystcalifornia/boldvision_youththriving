647b8f9e1b447af7894cafb89f1d4a5d294b4d15
Maria Khan <MKhan@catalystcalifornia.org>
2025-02-28 13:47:47 -0800

    adding a centralized styling script...

---
+++ Visuals/BV_styling.R
@@ -0,0 +1,33 @@
+#The purpose of this script is to centralized styling for ggplot functions in one place.
+
+#The following aim to follow the Bold Vision Style Guide stored here: 
+library(ggplot2)
+
+##Colors
+gray <- "#D6D7D6"
+pink <- "#F75EC1"
+dark_pink <- "#EF4A66"
+orange <- "#F57E20"
+yellow <- "#FFBF00"
+light_green <- "#00A75A"
+dark_green <- "#00864A"
+blue  <- "#2A12B2"
+light_blue <- "#465adc"
+
+
+## FONTS 
+font_add(family = "Manifold CF", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Manifold/Fonts/manifoldcf-heavy.otf")
+font_add(family = "HelveticaNeueLTStdMdCn", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-MdCn.otf")
+font_add(family = "HelveticaNeueLTStdHvCn", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-HvCn.otf")
+font_add(family = "HelveticaNeueLTStdMdCnO", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-MdCnO.otf")
+# font_import()
+loadfonts(device = "win")
+windowsFonts()
+showtext_auto()
+# define fonts in chart
+font_title <- "Manifold CF"
+font_subtitle <- "Manifold CF"
+font_caption <- "HelveticaNeueLTStdMdCn"
+font_bar_label <- "HelveticaNeueLTStdHvCn"
+font_axis_label <- "HelveticaNeueLTStdMdCn"
+
