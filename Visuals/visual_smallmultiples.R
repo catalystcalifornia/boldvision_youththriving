25de47eb3636411320bdb311ea3e627d2453cf93
Maria Khan <MKhan@catalystcalifornia.org>
2025-02-28 13:49:59 -0800

    using in scripts

--- Visuals/visual_smallmultiples.R
+++ Visuals/visual_smallmultiples.R
@@ -22,34 +22,8 @@ library(flextable)
 library(ggchicklet)
 library(GGRidge)
 
-##Colors
-gray <- "#D6D7D6"
-pink <- "#F75EC1"
-dark_pink <- "#EF4A66"
-orange <- "#F57E20"
-yellow <- "#FFBF00"
-light_green <- "#00A75A"
-dark_green <- "#00864A"
-blue  <- "#2A12B2"
-light_blue <- "#465adc"
-
-
-## FONTS 
-font_add(family = "Manifold CF", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Manifold/Fonts/manifoldcf-heavy.otf")
-font_add(family = "HelveticaNeueLTStdMdCn", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-MdCn.otf")
-font_add(family = "HelveticaNeueLTStdHvCn", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-HvCn.otf")
-font_add(family = "HelveticaNeueLTStdMdCnO", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-MdCnO.otf")
-# font_import()
-loadfonts(device = "win")
-windowsFonts()
-showtext_auto()
-# define fonts in chart
-font_title <- "Manifold CF"
-font_subtitle <- "Manifold CF"
-font_caption <- "HelveticaNeueLTStdMdCn"
-font_bar_label <- "HelveticaNeueLTStdHvCn"
-font_axis_label <- "HelveticaNeueLTStdMdCn"
-
+#Load BV styling, colors and fonts
+source('Visuals\\BV_styling.R')
 
 source("W:\\RDA Team\\R\\credentials_source.R")
 con <- connect_to_db("bold_vision")
