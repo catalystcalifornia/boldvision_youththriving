e65320e02755e48186915f5258afffae2a3280c6
Maria Khan <MKhan@catalystcalifornia.org>
2025-04-29 15:25:39 -0700

    updated script

--- Visuals/BV_styling.R
+++ Visuals/BV_styling.R
@@ -16,18 +16,31 @@ light_blue <- "#465adc"
 
 
 ## FONTS 
+font_add(family = "Manifold Regular", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Manifold/Fonts/manifoldcf-regular.otf")
 font_add(family = "Manifold CF", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Manifold/Fonts/manifoldcf-heavy.otf")
 font_add(family = "HelveticaNeueLTStdMdCn", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-MdCn.otf")
 font_add(family = "HelveticaNeueLTStdHvCn", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-HvCn.otf")
 font_add(family = "HelveticaNeueLTStdMdCnO", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-MdCnO.otf")
+font_add(family = "HelveticaNeueLTStdMd", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Helvetica Neue LT Std/HelveticaNeueLTStd-Md.otf")
+
 # font_import()
 loadfonts(device = "win")
 windowsFonts()
 showtext_auto()
 # define fonts in chart
-font_title <- "Manifold CF"
-font_subtitle <- "Manifold CF"
-font_caption <- "HelveticaNeueLTStdMdCn"
-font_bar_label <- "HelveticaNeueLTStdHvCn"
-font_axis_label <- "HelveticaNeueLTStdMdCn"
+font_title <- "HelveticaNeueLTStdHvCn"
+font_subtitle <- "HelveticaNeueLTStdMdCn"
+font_caption <- "Manifold Regular"
+font_bar_label <- "Manifold Regular"
+font_axis_label <- "Manifold Regular"
+
+
+## FONT SIZES 
+bar_label_fs <- 2.6
+lengend_text_fs <- 14
+legend_title_fs <- 12
+caption_fs <- 11
+title_fs <- 18
+subtitle_fs <- 15
+
 
