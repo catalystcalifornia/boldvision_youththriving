17c2e283999d8ee00f0fafbbdfb12f028893b6eb
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-05-02 12:32:28 -0700

    renaming the raw survey data so it isn't overwritten for checks later. Adding Alone to the Alone categories and adjusting values of AOIC categories so it's clear later which are Alone for alone or in combo, adjusting the function so it isn't leading to duplicate rows and also accurately estimating SE of combined responses. Running functions for AOIC categories. Adjusting column comments.

--- Descriptive and Demographic Analyses/analysis_asian_disag.R
+++ Descriptive and Demographic Analyses/analysis_asian_disag.R
@@ -14,7 +14,7 @@ source("W:\\RDA Team\\R\\credentials_source.R")
 con <- connect_to_db("bold_vision")
 
 #raw survey data 
-svy_data <- dbGetQuery(con, "SELECT * FROM youth_thriving.raw_survey_data")
+svy_data_raw <- dbGetQuery(con, "SELECT * FROM youth_thriving.raw_survey_data")
 #data dictionary
 svy_dd <- dbGetQuery(con, "SELECT * FROM youth_thriving.bvys_datadictionary_2024 where response_type = 'mc' AND response_domain !='Demographics' AND response_domain != 'Info'")
 #race data
@@ -27,7 +27,7 @@ race_df <- dbGetQuery(con, 'SELECT response_id, nh_race, race_asian, detailed_as
 #### STEP 2: Clean up your data and create a column that defines different Asian subgroups ####
 #join to have a df with the detailed asian categories and filter out any survey respondents that did not identify with this category
 svy_data <- race_df %>%
-  left_join(svy_data, by = "response_id")
+  left_join(svy_data_raw, by = "response_id")
 
 svy_data <- svy_data %>%
   mutate(
@@ -38,10 +38,10 @@ svy_data <- svy_data %>%
     subgroup_asian = case_when(
       another_race_asian ==  1 ~ "Multiracial" , # Asian and Another Race
       multiple_asian >  1 ~ "Multi-Asian", # More than one Asian subgroup
-      south_asian == 1 ~ "South Asian",
-      southeast_asian == 1 ~ "Southeast Asian",
-      east_asian == 1 ~ "East Asian",
-      central_asian == 1 ~ "Central Asian",
+      south_asian == 1 ~ "South Asian Alone",
+      southeast_asian == 1 ~ "Southeast Asian Alone",
+      east_asian == 1 ~ "East Asian Alone",
+      central_asian == 1 ~ "Central Asian Alone",
       race_asian == 1 ~ "Asian",
       TRUE ~ NA_character_
     ),
@@ -59,8 +59,25 @@ qa_3<-svy_data%>%group_by(detailed_asian,south_asian_aoic)%>%summarise(count=n()
 
 qa_4<-svy_data%>%group_by(detailed_asian,central_asian_aoic)%>%summarise(count=n())
 
+# Recode AOIC variables for easier reading later
+# list of binary vars it can apply to systematically
+binary_vars <- svy_data%>% 
+  select(southeast_asian_aoic:central_asian_aoic) %>%
+  names()
+
+# loop statement
+for (i in binary_vars) { 
+  svy_data <- svy_data %>%
+    mutate(!!paste({{i}}):=
+             case_when(
+               !!sym(i)==1 ~ str_to_title(gsub("_"," ",!!paste({{i}}))),
+               !!sym(i)==0 ~ str_to_title(gsub("_"," ",!!paste("not",{{i}}))),
+               TRUE ~ NA
+             ) )
+}
+
 ####STEP 3: Create a function for Asian subgroups ####
-fx_disagg_asian <- function(df_input, variable_input,
+fx_disagg_asian_orig <- function(df_input, variable_input,
                             low_input_a, low_input_b, low_rename,
                             high_input_a, high_input_b, high_rename) {
   
@@ -126,13 +143,107 @@ fx_disagg_asian <- function(df_input, variable_input,
   return(df_combined_responses)
 }
 
+fx_disagg_asian <- function(df_input, race_var, variable_input,
+                            low_input_a, low_input_b, low_rename,
+                            high_input_a, high_input_b, high_rename) {
+  
+  # Filter for the variable of interest
+  df <- df_input %>% select(!!sym(race_var), !!sym(variable_input),weights_final)
+      
+  # Load response dictionary for this variable
+  dict <- svy_dd %>%
+    filter(variable == variable_input) %>%
+    pivot_longer(cols = response_1:response_12,
+                 names_to = "response_number",
+                 values_to = "response",
+                 values_drop_na = TRUE) 
+  
+  dict <- dict %>%
+    mutate(variable_merge_col = 1:nrow(dict)) # for merging later
+  
+  # Merge dictionary
+  df_merge <- df %>%
+    left_join(dict, by = setNames("variable_merge_col", variable_input)) %>%  
+    relocate(response)
+  
+  # Combine response groups
+  df_combined_responses <- df_merge %>%
+    mutate(response_group = case_when(
+      response %in% c(low_input_a, low_input_b) ~ low_rename,
+      response %in% c(high_input_a, high_input_b) ~ high_rename,
+      TRUE ~ NA_character_
+    )) %>%
+    as_survey(weights = weights_final) %>%
+    group_by(!!sym(race_var), response_group) %>%
+    summarise(
+      count = n(),
+      rate = survey_mean(vartype = c("cv", "se"), level = 0.90),      
+      variable = first(variable),
+      question = first(question),
+      sub_question = first(sub_question),
+      question_number = first(question_number),
+      likert_type = first(likert_type),
+      variable_name = first(variable_name),
+      response_domain = first(response_domain),
+      .groups = 'drop'
+    ) %>%
+  filter(!is.na(response_group)) %>%
+    mutate(
+      rate = rate * 100,
+      rate_cv = rate_cv * 100,
+      rate_se = rate_se * 100,
+      moe = rate_se * 1.645
+    )
+    
+   return(df_combined_responses)
+}
+
 
 ####STEP 4: Run tables on variables of interest and check if they look okay ####
 
-disagg_asian_co <- fx_disagg_asian(svy_data, "co", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
+disagg_asian_co_alone <- fx_disagg_asian(svy_data, "subgroup_asian", "co", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
 
-disagg_asian_dl <- fx_disagg_asian(svy_data, "dl", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
+disagg_asian_co_se <- fx_disagg_asian(svy_data, "southeast_asian_aoic", "co", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
 
+disagg_asian_co_sa <- fx_disagg_asian(svy_data, "south_asian_aoic", "co", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
+
+disagg_asian_co_ca <- fx_disagg_asian(svy_data, "central_asian_aoic", "co", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
+
+disagg_asian_co<-rbind(disagg_asian_co_alone, 
+                       disagg_asian_co_se %>%
+                         rename(subgroup_asian=1), # rename columns for binding
+                       disagg_asian_co_sa %>%
+                         rename(subgroup_asian=1), # rename columns for binding
+                       disagg_asian_co_ca %>%
+                         rename(subgroup_asian=1)) %>% # rename columns for binding
+                      filter(!str_detect(subgroup_asian,"Not ")) # remove the not aoic matching categories
+
+disagg_asian_dl_alone <- fx_disagg_asian(svy_data, "subgroup_asian", "dl", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
+
+disagg_asian_dl_se <- fx_disagg_asian(svy_data, "southeast_asian_aoic", "dl", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
+
+disagg_asian_dl_sa <- fx_disagg_asian(svy_data, "south_asian_aoic", "dl", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
+
+disagg_asian_dl_ca <- fx_disagg_asian(svy_data, "central_asian_aoic", "dl", "Never true", "Sometimes true", "Sometimes/Never True", "Often true", "Always true", "Often/Always True")
+
+disagg_asian_dl <- rbind(disagg_asian_dl_alone, 
+                         disagg_asian_dl_se %>%
+                           rename(subgroup_asian=1), # rename columns for binding
+                         disagg_asian_dl_sa %>%
+                           rename(subgroup_asian=1), # rename columns for binding
+                         disagg_asian_dl_ca %>%
+                           rename(subgroup_asian=1)) %>% # rename columns for binding
+  filter(!str_detect(subgroup_asian,"Not ")) # remove the not aoic matching categories
+
+# qa check of survey function
+qa<- svy_data %>% as_survey_design(weights = weights_final) %>%
+  group_by(subgroup_asian, dl) %>%
+  summarise(count=n(), # participant records count
+            rate = survey_prop(vartype=c("cv","se"),level=.90)) %>% 
+  mutate(rate = rate * 100,
+         rate_se = rate_se * 100,
+         rate_cv=rate_cv*100)   # Convert to percentages
+# checks out
 
 ####STEP 5: Create function for writing data table to database ####
 
@@ -163,13 +274,15 @@ column_comments <- c(
   "count" = " count of youth that selected this response",
   "rate" = " rate of youth that selected this response",
   "rate_cv" = "A weighted coefficient of variation for this rate",
-  "moe" = "A weighted margin of error for this rate",
+  "rate_se" = "A weighted standard error for this rate",
   "variable" = "Refers to the column label or variable in the survey data",
   "question" = "The question that this variable refers to",
   "sub_question" = "The subquestion that this variable refers to",
+  "question_number" = "The question number of the variable",
   "likert_type" = "Likert scale type",
   "variable_name" = "The survey SUBcomponent this variable falls under",
-  "response_domain" = "The survey component this variable falls under"
+  "response_domain" = "The survey component this variable falls under",
+  "moe" = "A weighted margin of error for this rate"
 )
 
 add_table_comments(con, schema, table_name, indicator, source, qa_filepath, column_names, column_comments)
