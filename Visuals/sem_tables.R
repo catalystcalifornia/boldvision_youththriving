21dc7948585d90fea404a57183b1b83422762cfc
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-05-29 17:26:42 -0700

    script updates

--- Visuals/sem_tables.R
+++ Visuals/sem_tables.R
@@ -11,6 +11,7 @@ library(ggtext)
 library(RPostgres)
 library(gt)
 library(gtExtras)
+showtext_auto()
 
 
 # connect to postgres and source functions
@@ -44,7 +45,8 @@ sem_sig_df <- sem_df %>% filter(p_is_significant) %>%
     str_detect(rhs_label, "Microaggressions") ~ "Freedom From Microaggressions",  
     str_detect(rhs_label, "Structural Racism") ~ "Freedom From Structural Racism", 
     TRUE ~ rhs_label
-  ))
+  )) %>%
+  mutate(std.all=std.all*-1)
          
 # check
   sem_sig_df$rhs_label         
@@ -60,36 +62,45 @@ df <- sem_sig_df %>%
   mutate(
     icon = ifelse(std.all >= 0, "\u25B2", "\u25BC"),
     direction = ifelse(std.all >= 0, "positive", "negative")
-  )
+  ) %>%
+    mutate(
+      y_pos = as.numeric(factor(rhs_label, levels = rev(unique(rhs_label)))) * 2  # multiply by 2 to double spacing
+    )
 
-ggplot(df, aes(y = reorder(rhs_label, abs(std.all)))) +
-  geom_text(aes(x=1,label=rhs_label),hjust=0,size=12,family= font_axis_label) +
-  geom_text(aes(x=3,label = icon, color = abs(std.all), size=50)) +
+  n_rows <- length(unique(df$rhs_label))
+  
+  line_positions <- seq(min(df$y_pos) - 1, max(df$y_pos) - 1, by = 2)
+  
+
+table <- ggplot(df, aes(y = y_pos)) +
+  geom_text(aes(x=.9,label=rhs_label),hjust=0,size=3.9,family= font_axis_label,colour = "black") +
+  geom_text(aes(x=.85,label = icon, color = abs(std.all), size=5.2)) +
+  geom_hline(yintercept = line_positions, color = gray, size = 0.1) +
   # scale_color_manual(
   #   values = c(
   #     "positive" = pink,
   #     "negative" = light_blue
   #   )
   # ) + 
-  scale_color_gradient(low = "#FDDFF3", high = "#F75EC1") +
+  scale_color_gradient2(low = "#FA9EDA", mid= "#F75EC1", high = "#943874") +
   # Hide default y-axis labels and gridlines
-  scale_x_continuous(limits = c(0.8, 3.2), breaks = NULL) +
+  scale_x_continuous(limits = c(0.8, 1.5), expand = c(0, 0), breaks = NULL) +
+  scale_y_continuous(
+    breaks = df$y_pos,
+    labels = df$rhs_label,
+    expand = expansion(add = 1)
+  ) +
   labs(
-    title = "Drivers of Freedom of Psychological Distress", 
+    title = "Drivers of Psychological Distress", 
     subtitle = paste
     ("Ordered from greatest to least effect",
-      "\u25B2 indicates a positive effect",
-      "\u25BC indicates a negative effect",
       sep = "\n"
     )
-    # ,
-    # caption = paste("\nCatalyst California's calculations of Bold Vision Youth Thriving Survey, 2024.",
-    #                 "Note: AIAN=American Indian & Alaska Native; BIPOC=Black, Indigenous, People of Color;", 
-    #                 "LGBTQIA+=Lesbian, Gay, Bisexual, Transgender, Queer, Intersex, Asexual, & Gender", 
-    #                 "Nonconforming; NHPI: Native Hawaiian & Pacific Islander; SWANA=Southwest Asian & North",
-    #                 "African; Systems Impacted=Youth at any point in foster care, juvenile hall/probation camp",
-    #                 "jail/prison, group home/residential program, or lived with legal guardians.",
-    #                 sep="\n")
+    ,
+    caption = paste("up arrow means an increase in psychological distress", 
+                    "down arrow means a decrease in psychological distress",
+                    "All effects are statistically significant at the p<0.05 level",
+                    sep="\n")
     ) +
   theme_minimal() +
   theme(
@@ -98,32 +109,21 @@ ggplot(df, aes(y = reorder(rhs_label, abs(std.all)))) +
     axis.ticks.y = element_blank(),
     panel.grid = element_blank(),
     legend.position = "none",
-    # legend.title = element_text(hjust = 0.5,size = 12, family= font_axis_label),
-    #                                                                      legend.text = element_text(hjust = 0.5,size = 12, family= font_axis_label),
-    #                                                                      legend.position = "bottom", # no legend title
-    #                                                                      legend.margin=margin(l = 0),
-    #                                                                      # legend.margin=margin(-2,-2,-2,-2),
-    #                                                                      # legend.box.margin=margin(-2,-2,-2,-2),
-    #                                                                      # define style for axis text
-    #                                                                      axis.text.y=element_blank(),
-    #                                                                      # axis.text.y = element_text(size = 9, colour = "black", family= font_axis_label, face = "bold"),
-    #                                                                      axis.text.x=element_blank(),
-    #                                                                      axis.ticks=element_blank(),
-    #                                                                      axis.ticks.length = unit(0, "pt"),
-    #                                                                      # axis.text.x=element_text(size = 11, colour = "black", family = font_axis_label),
-    #                                                                      axis.title.x=element_blank(),
-    #                                                                      # axis.title.x = element_text(size = 12, colour = "black", family = font_axis_label, face = "bold"),
-    #                                                                      # define style for title and caption
-    #                                                                      plot.caption = element_text(hjust = 0.0, size = 10, colour = "black", family = font_caption),
-                                                                         plot.subtitle =
-                                                                           element_text(hjust = 0.0, size = 15, family = font_subtitle),
-                                                                         plot.title =
-                                                                           element_text(hjust = 0.0, size = 18, family = font_title)
-    #                                                                      # ,
-    #                                                                      #   element_text(hjust = 0.0, size = 20, colour = "black", family = font_title)
-    #                                                                      , 
-    #                                                                      # grid line style
-    #                                                                      panel.border=element_blank(),
-    #                                                                      panel.grid = element_blank()
-    
+     # define style for title and caption
+   plot.caption = element_text(hjust = 0.0, size = 9, colour = "black", family = font_caption),
+   plot.subtitle =element_text(hjust = 0.0, size = 12, colour = "black",family = font_subtitle),
+   plot.title =element_text(hjust = 0.0, size = 14, colour = "black",family = font_title),
+   panel.background = element_rect(fill = "#F5F5F5", color = NA),  # light gray background
+   plot.background = element_rect(fill = "white", color = NA),     # keep outer plot white
     )
+
+
+showtext_opts(dpi=300)
+
+# ggsave(plot=table, 
+#        file="./Visuals/Strong Minds/sem_table_Psychological_Distress.png",
+#        units = c("in"),  width = 3, height = 2, dpi=300)
+
+ggsave(plot=table,
+       file="./Visuals/Strong Minds/sem_table_Psychological_Distress.pdf", device = cairo_pdf,
+       units = c("in"),  width = 4, height = 3)
