6289397c865008e9e101668a151986f5d774d285
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-06-23 21:33:55 -0700

    changes to sem tables based on QA

--- Visuals/sem_tables.R
+++ Visuals/sem_tables.R
@@ -22,6 +22,12 @@ con <- connect_to_db("bold_vision")
 # Load BV styling, colors and fonts
 source('Visuals\\BV_styling.R')
 
+# add special fonts
+font_add(family = "Manifold Bold", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Manifold/Fonts/manifoldcf-bold.otf")
+font_add(family = "Manifold Extra Bold", regular = "W:/Project/OSI/Bold Vision/BV 2021/Deliverables/Bold Vision Fonts/Manifold/Fonts/manifoldcf-extrabold.otf")
+font_icon <- "Manifold Bold"
+font_icon_heavy <- "Manifold Extra Bold"
+
 # Step 1: Prepare data ----
 # load table and filter for final models
 sem_df <- dbGetQuery(con, "SELECT * FROM youth_thriving.factor_analysis_sem_results_weighted 
@@ -71,36 +77,29 @@ sem_sig_df <- sem_df %>% filter(p_is_significant) %>%
 # Reverse coefficients and prepare the data with arrow
 df <- df %>%
   mutate(std=ifelse(rhs_label %in% reverse_c | rhs_label %in% reverse_d, std.all*-1, std.all), # reverse
-    icon = ifelse(std >= 0, "+", "\u2212"), # add + or -, - code is for nicer looking minus sign
+    icon = ifelse(std >= 0, "+", "\u2212"), 
     direction = ifelse(std >= 0, "increase", "decrease") # indicate increase or decrease
   ) %>%
     mutate(
-      y_pos = as.numeric(factor(rhs_label, levels = rev(unique(rhs_label)))) * 2.2  # multiply by 2 to have double spacing in the table
+      y_pos = as.numeric(factor(rhs_label, levels = rev(unique(rhs_label)))) * 2.5  # multiply by 2 to have double spacing in the table
     )
 
 df <- df %>% select(rhs_label, std, icon, direction, y_pos) # reduce columns
 
-  # n_rows <- length(unique(df$rhs_label)) # step to add lines between rows
+  line_positions <- seq(min(df$y_pos) - 1, max(df$y_pos) + 2.5, by = 2.5) # line positions with the double spacing
   
-  line_positions <- seq(min(df$y_pos) - 1, max(df$y_pos) + 2.2, by = 2.2) # line positions with the double spacing
-  
-  header_y <- max(df$y_pos) + 2.2  # push it above the top row # add header row
+  header_y <- max(df$y_pos) + 2.5  # push it above the top row # add header row
   
 table <- ggplot(df, aes(y = y_pos)) + # rows with double spacing
-  geom_text(aes(x=.95,label=rhs_label),hjust=0,size=3.9,family= font_axis_label,colour = "black") + # coefficient label
+  geom_text(aes(x=.95,label=rhs_label),hjust=0,size=4.2,family= font_axis_label,colour = "black") + # coefficient label
   geom_text(aes(x = 0.85, label = icon), # outline color for arrow
-            color = "grey22",  
-            size = 5.2 + 0.2  # slightly bigger than top layer
+            color = "black",  
+            size = 8 + 0.2,  # slightly bigger than top layer
+            family=font_icon_heavy
            ) +
-  geom_text(aes(x=.85,label = icon, color = abs(std), size=5.2)) + # arrow and then color based on absolute value
+  geom_text(aes(x=.85,label = icon, color = abs(std)), size=8, family=font_icon) + # arrow and then color based on absolute value
   geom_hline(yintercept = line_positions, color = gray, size = 0.1) + # lines between rows
-  # scale_color_manual(
-  #   values = c(
-  #     "positive" = pink,
-  #     "negative" = light_blue
-  #   )
-  # ) + 
-  scale_color_gradient2(low = "#FDE1F3", high = "#F75EC1") + # color gradient for as coefficient goes from weaker to stronger effect (based on absolute value)
+  scale_color_gradient2(low = "#FCAEDC", high = "#F75EC1") + # color gradient for as coefficient goes from weaker to stronger effect (based on absolute value)
   scale_x_continuous(limits = c(0.8, 1.5), expand = c(0, 0), breaks = NULL) + # x limits
   scale_y_continuous(
     breaks = df$y_pos,
@@ -109,11 +108,11 @@ table <- ggplot(df, aes(y = y_pos)) + # rows with double spacing
   ) +
   labs(
     title = "Psychological Distress Predictors", 
-    subtitle = "Ordered from <span style ='color: #F75EC1;'>largest</span> to <span style ='color: #FFD7EE;'>smallest </span>effect",
-    caption = paste("+ means an increase in psychological distress",
-                    "- means a decrease in psychological distress",
-                    "All effects are statistically significant at the p<0.05 level. For more information", 
-                    "on the methodology, please refer to the 2025 Bold Vision Youth Thriving report.",
+    subtitle = "Ordered from <span style ='color: #F75EC1;'>largest</span> to <span style ='color: #FCAEDC;'>smallest </span>effect",
+    caption = paste("+ means a worsening effect, or an increase in psychological distress.",
+                    "- means a protective effect, or a decrease in psychological distress.",
+                    "All effects are statistically significant at the p<0.05 level and ordered by absolute value.", 
+                    "For more information, please refer to the 2025 Bold Vision Youth Thriving report.",
                     sep="\n")
                     
     ) +
@@ -125,28 +124,28 @@ table <- ggplot(df, aes(y = y_pos)) + # rows with double spacing
     panel.grid = element_blank(),
     legend.position = "none",
      # define style for title and caption
-   plot.caption = element_text(hjust = 0.0, size = 9, colour = "black", family = font_caption),
-   plot.subtitle =element_markdown(hjust = 0.0, size = 12, colour = "black",family = font_subtitle),
-   plot.title =element_text(hjust = 0.0, size = 14, colour = "black",family = font_title),
+   plot.caption = element_text(hjust = 0.0, size = caption_fs, colour = "black", family = font_caption),
+   plot.subtitle =element_markdown(hjust = 0.0, size = subtitle_fs, colour = "black",family = font_subtitle),
+   plot.title =element_text(hjust = 0.0, size = title_fs, colour = "black",family = font_title),
    panel.background = element_rect(fill = "#F5F5F5", color = NA),  # light gray background
    plot.background = element_rect(fill = "white", color = NA),     # keep outer plot white
     ) +
   # header row
   annotate("text", x = 0.85, y = header_y, label = "Effect", hjust = 0.4, 
-           family = font_axis_label, size = 4, fontface = "bold") +
+           family = font_axis_label, size = 4.2, fontface = "bold") +
   annotate("text", x = 0.95, y = header_y, label = "Predictor", hjust = 0,
-           family = font_axis_label, size = 4, fontface = "bold")
+           family = font_axis_label, size = 4.2, fontface = "bold")
 
 
 showtext_opts(dpi=300)
 
-# ggsave(plot=table,
-#        file="./Visuals/Strong Minds/sem_table_Psychological_Distress.png",
-#        units = c("in"),  width = 4, height = 3, dpi=300)
-# 
-# ggsave(plot=table,
-#        file="./Visuals/Strong Minds/sem_table_Psychological_Distress.pdf", device = cairo_pdf,
-#        units = c("in"),  width = 4, height = 3)
+ggsave(plot=table,
+       file="./Visuals/Strong Minds/sem_table_Psychological_Distress.png",
+       units = c("in"),  width = 6, height = 4, dpi=300)
+
+ggsave(plot=table,
+       file="./Visuals/Strong Minds/sem_table_Psychological_Distress.pdf", device = cairo_pdf,
+       units = c("in"),  width = 6, height = 4)
 
 
 
@@ -161,48 +160,42 @@ df <- sem_sig_df %>%
 # Prepare the data with arrow
 df <- df %>%
   mutate(std=std.all, 
-         icon = ifelse(std >= 0, "\u25B2", "\u25BC"), # add arrow
+         icon = ifelse(std >= 0, "+", "\u2212"), 
          direction = ifelse(std >= 0, "increase", "decrease") # indicate increase or decrease
   ) %>%
   mutate(
-    y_pos = as.numeric(factor(rhs_label, levels = rev(unique(rhs_label)))) * 2.2  # multiply by 2 to have double spacing in the table
+    y_pos = as.numeric(factor(rhs_label, levels = rev(unique(rhs_label)))) * 2.5  # multiply by 2 to have double spacing in the table
   )
 
 df <- df %>% select(rhs_label, std, icon, direction, y_pos) # reduce columns
 
-n_rows <- length(unique(df$rhs_label)) # step to add lines between rows
+line_positions <- seq(min(df$y_pos) - 1, max(df$y_pos) + 2.5, by = 2.5) # line positions with the double spacing
 
-line_positions <- seq(min(df$y_pos) - 1, max(df$y_pos) + 2.2, by = 2.2) # line positions with the double spacing
-
-header_y <- max(df$y_pos) + 2.2  # push it above the top row # add header row
+header_y <- max(df$y_pos) + 2.5  # push it above the top row # add header row
 
 table <- ggplot(df, aes(y = y_pos)) + # rows with double spacing
-  geom_text(aes(x=.95,label=rhs_label),hjust=0,size=3.9,family= font_axis_label,colour = "black") + # coefficient label
+  geom_text(aes(x=.95,label=rhs_label),hjust=0,size=4.2,family= font_axis_label,colour = "black") + # coefficient label
   geom_text(aes(x = 0.85, label = icon), # outline color for arrow
-            color = "grey22",  
-            size = 5.2 + 0.2  # slightly bigger than top layer
+            color = "black",  
+            size = 8 + 0.2,  # slightly bigger than top layer
+            family=font_icon_heavy
   ) +
-  geom_text(aes(x=.85,label = icon, color = abs(std), size=5.2)) + # arrow and then color based on absolute value
+  geom_text(aes(x=.85,label = icon, color = abs(std)), size=8, family=font_icon) + # arrow and then color based on absolute value
   geom_hline(yintercept = line_positions, color = gray, size = 0.1) + # lines between rows
-  # scale_color_manual(
-  #   values = c(
-  #     "positive" = pink,
-  #     "negative" = light_blue
-  #   )
-  # ) + 
-  scale_color_gradient2(low = "#FDD8DD", high = "#EF4A66") + # color gradient for as coefficient goes from weaker to stronger effect (based on absolute value)
+  scale_color_gradient2(low = "#F9A3AF", high = "#EF4A66") + # color gradient for as coefficient goes from weaker to stronger effect (based on absolute value)
   scale_x_continuous(limits = c(0.8, 1.5), expand = c(0, 0), breaks = NULL) + # x limits
   scale_y_continuous(
     breaks = df$y_pos,
     labels = df$rhs_label,
     expand = expansion(add = 1)
   ) +
-  labs(
+ labs(
     title = "Self-Efficacy And Hope Predictors", 
-    subtitle = "Ordered from <span style ='color: #EF4A66;'>greatest</span> to <span style ='color: #FDD8DD;'>lowest </span>effect",
-    caption = paste("up arrow means an increase in self-efficacy and hope",
-                    "down arrow means a decrease in self-efficacy and hope",
-                    "All effects are statistically significant at the p<0.05 level",
+    subtitle = "Ordered from <span style ='color: #EF4A66;'>largest</span> to <span style ='color: #F9A3AF;'>smallest </span>effect",
+    caption = paste("+ means a protective effect, or an increase in self-efficacy and hope.",
+                    "- means a worsening effect, or a decrease in self-efficacy and hope.",
+                    "All effects are statistically significant at the p<0.05 level and ordered by absolute value.", 
+                    "For more information, please refer to the 2025 Bold Vision Youth Thriving report.",
                     sep="\n")
     
   ) +
@@ -214,26 +207,26 @@ table <- ggplot(df, aes(y = y_pos)) + # rows with double spacing
     panel.grid = element_blank(),
     legend.position = "none",
     # define style for title and caption
-    plot.caption = element_text(hjust = 0.0, size = 9, colour = "black", family = font_caption),
-    plot.subtitle =element_markdown(hjust = 0.0, size = 12, colour = "black",family = font_subtitle),
-    plot.title =element_text(hjust = 0.0, size = 14, colour = "black",family = font_title),
+    plot.caption = element_text(hjust = 0.0, size = caption_fs, colour = "black", family = font_caption),
+    plot.subtitle =element_markdown(hjust = 0.0, size = subtitle_fs, colour = "black",family = font_subtitle),
+    plot.title =element_text(hjust = 0.0, size = title_fs, colour = "black",family = font_title),
     panel.background = element_rect(fill = "#F5F5F5", color = NA),  # light gray background
     plot.background = element_rect(fill = "white", color = NA),     # keep outer plot white
   ) +
   # header row
   annotate("text", x = 0.85, y = header_y, label = "Effect", hjust = 0.4, 
-           family = font_axis_label, size = 4, fontface = "bold") +
+           family = font_axis_label, size = 4.2, fontface = "bold") +
   annotate("text", x = 0.95, y = header_y, label = "Predictor", hjust = 0,
-           family = font_axis_label, size = 4, fontface = "bold")
+           family = font_axis_label, size = 4.2, fontface = "bold")
 
 
 showtext_opts(dpi=300)
 
 ggsave(plot=table,
        file="./Visuals/Positive Identity and Self-Worth/sem_table_Self_Efficacy_And_Hope.png",
-       units = c("in"),  width = 4, height = 3, dpi=300)
+       units = c("in"),  width = 6, height = 4, dpi=300)
 
 ggsave(plot=table,
        file="./Visuals/Positive Identity and Self-Worth/sem_table_Self_Efficacy_And_Hope.pdf", device = cairo_pdf,
-       units = c("in"),  width = 4, height = 3)
+       units = c("in"),  width = 6, height = 4)
 
