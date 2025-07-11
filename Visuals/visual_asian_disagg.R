ccb0dff3025dce69ef79348733ec42a34dbf4fbc
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-07-11 09:28:25 -0700

    Edits to Asian disaggregation chart -- labeling, legend margins, and caption

--- Visuals/visual_asian_disagg.R
+++ Visuals/visual_asian_disagg.R
@@ -30,7 +30,12 @@ df <- dbGetQuery(con, "SELECT * FROM youth_thriving.asian_disagg_co WHERE subgro
   'East Asian Aoic',
   'South Asian Aoic',
   'Southeast Asian Aoic') ") %>%
-  mutate(subgroup_asian = gsub(" Alone", "", subgroup_asian))  #taking away alone
+  mutate(subgroup_asian = gsub(" Alone", "", subgroup_asian),  #taking away alone
+         subgroup_asian = case_when(
+           subgroup_asian=='Multiracial' ~ 'Multiracial Asian', # recoding categories for clarity and better representation
+           subgroup_asian=='Multi-Asian' ~ 'Multiethnic Asian', # recoding categories for clarity and better representation
+           TRUE ~ subgroup_asian
+         ))
   
   order_levels <- df %>%
   filter(response_group == "Often/Always True") %>%
@@ -38,8 +43,15 @@ df <- dbGetQuery(con, "SELECT * FROM youth_thriving.asian_disagg_co WHERE subgro
   pull(subgroup_asian)
 
 df <- df %>%
-  mutate(subgroup_asian = factor(subgroup_asian, levels = order_levels))
-
+  mutate(subgroup_asian = factor(subgroup_asian, levels = order_levels),
+         label = case_when(
+           count <= 5 ~ NA, # threshold to leave out any data has a count of 5 or less
+           rate_cv > 40 ~ paste0(round(rate, 0), "%*"),
+           TRUE ~ paste0(round(rate, 0), "%")
+         ),
+         rate=case_when(
+           count <= 5 ~ NA, # threshold to leave out any data has a count of 5 or less
+           TRUE ~ rate))
 
 
 #### Step 4: Run Visual ####
@@ -47,19 +59,22 @@ df_visual <- ggplot(df, aes(x = subgroup_asian, y = rate, fill = response_group 
   geom_bar(stat = "identity", width = 0.8, position = "dodge") + 
   # bar labels
   geom_text(data = df,
-            aes(label = paste0(round(rate, digits = 1), "%")),
+            aes(label = label),
             size = 2.75,
             stat="identity", colour = "black",
             position = position_dodge(width = 1), 
             vjust = -0.1 ,
             family=font_bar_label) +  
-  labs(title = paste(str_wrap("Multi-Asian and East Asian youth are least likely to feel hopeful about their future among all Asian youth", whitespace_only = TRUE, width = 55), collapse = "\n"),
+  labs(title = paste(str_wrap("Multiethnic and East Asian youth are least likely to feel hopeful about their future among all Asian youth", whitespace_only = TRUE, width = 55), collapse = "\n"),
       subtitle = paste("Survey Question: I feel hopeful about my future"),
         x = "",
        y = "",
        fill = "",
-       caption= paste(str_wrap(paste0("Data Source: Catalyst California calculations of Bold Vision Youth Thriving Survey, 2024."),
-                               whitespace_only = TRUE, width = 120), collapse = "\n")) +
+       caption= paste(str_wrap(paste0("Data Source: Catalyst California's calculations of Bold Vision Youth Thriving Survey, 2024. ", 
+       "Groups with fewer than five individuals are omitted for privacy purposes. ",
+        "For more information on each group's definition, 
+         please refer to the 2025 Bold Vision Youth Thriving report."),
+                               whitespace_only = TRUE, width = 100), collapse = "\n")) +
   #theme/aesthetics
   theme_minimal() +
   theme(legend.position = "top",  # Show legend on the top/bottom
@@ -72,9 +87,13 @@ df_visual <- ggplot(df, aes(x = subgroup_asian, y = rate, fill = response_group 
         legend.text = element_text(size = 12, colour = "black", family = font_subtitle, 
                                    # face = "bold",
                                    margin = margin(t = 5)),
-        legend.title = element_text(size = 12, colour = "black", family = font_subtitle, 
-                                    # face = "bold",
-                                    margin = margin(t = 5)),
+        legend.title = element_text(size = 12, colour = "black", family = font_subtitle
+                                    # , 
+                                    # # face = "bold",
+                                    # margin = margin(t = 5)
+                                    ),
+        legend.margin=margin(-6,0,-2,0),
+        legend.box.margin=margin(-6,0,-2,0),
         # define style for title and caption
         plot.caption = element_text(hjust = 0.0, size = 11, colour = "black", family = font_caption, face = "plain"),
         plot.title = element_text(hjust = 0.0, size = 18, colour = "black", family = font_title),
@@ -93,4 +112,4 @@ df_visual <- ggplot(df, aes(x = subgroup_asian, y = rate, fill = response_group 
 ggsave(plot=df_visual, 
        file=paste0("./Visuals/", "/Positive Identity and Self-Worth/",
                    "asian_disagg_co", ".pdf"),
-       device = "pdf", units = c("in"),  width = 6, height = 3.5)
+       device = "pdf", units = c("in"),  width = 6, height = 4)
