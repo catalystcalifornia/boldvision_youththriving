69968c00090dcf1758be1af6e16aecf18f2b3de4
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-12-04 22:54:07 -0800

    Finished data screening

--- Data Prep and Quality/Data Sharing/data_screening.R
+++ Data Prep and Quality/Data Sharing/data_screening.R
@@ -37,8 +37,7 @@ screened_sogi_data <- sogi
 screened_race_data <- race
 screened_systems_data <- systems
 
-# Step 1: Screen raw survey data ----
-### Sex at birth screen -----
+### Step 1: Sex at birth screen -----
 # Include Sex at Birth responses (q22) but recode Don’t Wish to Answer and Other to *
 # dictionary reference
 dict %>% filter(variable=="q22")
@@ -57,7 +56,7 @@ screened_svy_data <- screened_svy_data %>%
 table(screened_svy_data$q22,useNA='always')
 table(raw_svy_data$q22,useNA='always')
 
-### SOGI screen -----
+### Step 2: SOGI screen -----
 # Omit original SOGI questions from provided data and special request data
 # data dictionary for gender and sexuality vars
 omit_sogi_vars <- dict %>% filter(variable_name %in% c("Gender","Sexual Orientation"))
@@ -79,7 +78,7 @@ screened_sogi_data <- screened_sogi_data %>%
 screened_sogi_data <- screened_sogi_data %>%
   select(-c(detailed_gender,detailed_sexuality))
 
-### Race data screen -----
+### Step 3: Race data screen -----
 race_vars <- dict %>% filter(variable_category=='race')
 
 # drop race vars from the svy data as we'll send our cleaned variables in race_ethnicity_data
@@ -158,7 +157,7 @@ screened_race_data <- screened_race_data %>%
   ))
 
 # check
-sum(screen_race$n)
+sum(screen_asian$n)
 screened_race_data %>% filter(detailed_asian=="Another Asian Subgroup Alone or In Combination (suppressed)") %>% count(detailed_asian)
 
 # Omit bh_clean, bh_original from provided data and from special request data, variable #bh includes just a 1/0 response, keep that column
@@ -168,34 +167,115 @@ screened_race_data <- screened_race_data %>%
   select(-all_of(omit_asian))
 
 ##### NHPI race categories ----
-# Omit br_clean, br_original from provided data and from special request data
-# Recode br (from race_ethnicity_data) to just include 1/0 for Other in special request data
-# Recode bk (from race_ethnicity_data) to Other in special request data
+# Omit br_clean, br_original from provided data and from special request data and recode br to just include 1/NA for Other in special request data
 # Drop bs (from race_ethnicity_data) in special request data
-# Recode bl (from race_ethnicity_data) to Other in special request data
-# Recode br (from race_ethnicity_data) to Other in special request data
-# NHPI detailed_nhpi responses - but recode <=10 to Other 
-# Special request data should include recoded responses from race_ethnicity_data rather than the original data
+omit_nhpi_vars<-c("br_clean","br_original","bs")
 
+screened_race_data <- screened_race_data %>%
+  mutate(br=case_when(
+    !is.na(br_clean) ~1,
+    TRUE ~ NA
+  )) %>%
+  select(-all_of(omit_nhpi_vars))
 
-## Systems involvement -- by special request
-# q24, q24a, q27, and q28 by special request with recoding by special request
-# Recoded variables - undocumented, systems_impacted,unhoused by special request
-# Recode specified Other write in for system involvement (q24a -> gg) -- generalize to other
-# Drop don't wish to answer from system involvement (q24a -> gf)
-# Omit value of 5 from immigration (q27) -- make *
-# Omit don't wish to answer from unhoused status value 4 from (q28) -- make *
+sum(!is.na(race$br_clean))
+sum(!is.na(screened_race_data$br))
 
-### Other demographics screen ------
-# Omit write-ins to the employment and education status write-in question (bz) -- generalize to Other
+# Recode bk, bl, and br to Other in special request data and drop original columns
+omit_nhpi_vars<-c("bk","bl")
+
+screened_race_data <- screened_race_data %>%
+  mutate(br=case_when(
+    !is.na(bk) ~1,
+    !is.na(bl) ~1,
+    TRUE ~ br
+  )) %>%
+  select(-all_of(omit_nhpi_vars))
+
+sum(!is.na(race$bk))
+sum(!is.na(race$bl))
+sum(!is.na(screened_race_data$br))
+
+# Include NHPI detailed_nhpi responses in special request data- but recode <=10 to Other 
+screen_nhpi <- screened_race_data %>% count(detailed_nhpi) %>% filter(n<=10)
+
+# convert values below 10 to -999
+screened_race_data <- screened_race_data %>%
+  mutate(detailed_nhpi=case_when(
+    detailed_nhpi %in% screen_nhpi$detailed_nhpi ~ "Another NHPI Subgroup Alone or In Combination (suppressed)",
+    TRUE ~ detailed_nhpi
+  ))
+
+# check
+sum(screen_nhpi$n)
+screened_race_data %>% filter(detailed_nhpi=="Another NHPI Subgroup Alone or In Combination (suppressed)") %>% count(detailed_nhpi)
+
+### Step 4: Systems involvement screen ------
+systems_vars <- dict %>% filter(variable_name %in% c("System Involvement","Unhoused"))
+
+# Recode specified Other write in for system involvement (q24a -> var gg) -- generalize to other
+# Drop don't wish to answer from system involvement (q24a -> var gf)
+omit_systems <- c("gf")
+
+screened_svy_data <- screened_svy_data %>%
+  mutate(gg=case_when(
+    !is.na(gg) ~1,
+    TRUE ~ NA
+  )) %>%
+  select(-all_of(omit_systems))
+
+sum(!is.na(raw_svy_data$gg))
+sum(!is.na(screened_svy_data$gg))
+
+# Omit value of 5 from immigration (q27) -- make -999
+# Omit don't wish to answer from unhoused status value 4 from (q28) -- make -999
+screened_svy_data <- screened_svy_data %>%
+  mutate(q27=case_when(
+    q27==5 ~ -999,
+    TRUE ~ q27),
+  q28=case_when(
+    q28==4 ~ -999,
+    TRUE ~ q28))
+
+# check
+table(raw_svy_data$q27)
+table(screened_svy_data$q27)
+
+# Recode value of 3 (Don’t wish to answer) from q25 “Q25. At any point, have you ever been detained AND/OR arrested by law enforcement in any capactiy?" to -999
+# Recode value of 3 (don’t wish to answer) from q26 “Q. 26. At any point, have you ever been suspended, experienced an \"opportunity transfer\", been expelled, have been…” to -999
+screened_svy_data <- screened_svy_data %>%
+  mutate(q25=case_when(
+    q25==3 ~ -999,
+    TRUE ~ q25),
+    q26=case_when(
+      q26==3 ~ -999,
+      TRUE ~ q26))
+
+# check
+table(raw_svy_data$q26)
+table(screened_svy_data$q26)
+
+### Step 5: Other demographics screen ------
 # Numerical age omitted entirely from provided and special request data
-# Drop cf Don’t Wish to Answer response to "Q7. I am a full-time or part-time student. Right now I am in:" 
-# Recode value of 3 (Don’t wish to answer) from q25 “Q25. At any point, have you ever been detained AND/OR arrested by law enforcement in any capactiy?" to *
-# Recode value of 3 (don’t wish to answer) from q26 “Q. 26. At any point, have you ever been suspended, experienced an \"opportunity transfer\", been expelled, have been…” to *
+screened_svy_data <- screened_svy_data %>% select(-numerical_age)
 
-### Geo data screen -----
+#### Employment and education status ----
+empl_educ_vars <- dict %>% filter(grepl("Education",variable_name))
+
+# Omit write-ins to the employment and education status write-in question (bz) -- generalize to Other 1/NA
+screened_svy_data <- screened_svy_data %>%
+  mutate(bz=case_when(
+    !is.na(bz) ~1,
+    TRUE ~ NA))
+
+sum(!is.na(raw_svy_data$bz))
+sum(!is.na(screened_svy_data$bz))
+
+# Drop cf Don’t Wish to Answer response to "Q7. I am a full-time or part-time student. Right now I am in:" 
+screened_svy_data <- screened_svy_data %>% select(-cf)
+
+### Step 6: Geo data screen -----
 # ZIP Code (zipcode_clean_respondent) by special request, but <=10 counts recoded as -999
-# Omit org_spa, org_sp_an, q20 (old zip code column) variable entirely from provided and special request data
 # dictionary reference
 zip_code_vars <- dict %>% filter(grepl("ZIP", question, ignore.case=TRUE))
 zip_code_vars$variable
@@ -215,9 +295,21 @@ screened_svy_data <- screened_svy_data %>%
 table(screened_svy_data$zipcode_clean_respondent,useNA='always')
 table(raw_svy_data$zipcode_clean_respondent,useNA='always')
 
+# Omit org_spa, org_sp_an, q20 (old zip code column) variable entirely from provided and special request data
 # remove original ZIP Code var and SPA vars from dataset
-screened_svy_data <- screened_svy_data %>% select(-c(q20,org_spa, org_sp_an))
+screened_svy_data <- screened_svy_data %>% select(-c(q20,org_spa, org_sp_an,org))
 
-## Geo demographics -- special request
-# Organization and ZIP Code available by special request
-# Omit org where count <=10 make *
+
+# Omit org where count <=10 make -999
+screen_org <- screened_svy_data %>% count(org) %>% filter(n<=10)
+
+# convert values below 10 to -999
+screened_svy_data <- screened_svy_data %>%
+  mutate(org=case_when(
+    org %in% screen_org$org ~ "Another Collector (suppressed)",
+    TRUE ~ org
+  ))
+
+# check
+sum(screen_org$n)
+screened_svy_data %>% filter(org=="Another Collector (suppressed)") %>% count(org)
